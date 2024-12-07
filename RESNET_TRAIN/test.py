ad77ae1a70d3374df0a0ec7a3fdf042c91c65708
candycca <42653475+candycca@users.noreply.github.com>
2024-12-07 20:48:23 +0800

    Update test.py

--- RESNET_TRAIN/test.py
+++ RESNET_TRAIN/test.py
@@ -18,7 +18,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from ResNet.dataLoad import make_test_dataloader
-from ResNet.dataLoad import make_actual_dataloader
+
 
 class_names = ['left', 'right', 'straight']
 
@@ -36,41 +36,6 @@ def predict_test_data(model, test_loader):
             predictions.extend([class_names[p] for p in predicted.cpu().numpy()])
 
     return predictions
-
-def predict(test_data_path,output_path,name):
-    weight_path = "/home/ai113/code2/ResNet/weights/weight.pth"
-    # load model and use weights we saved before
-    model = ResNet(ResidualBlock, [3,4,6,3])
-    model.load_state_dict(torch.load(weight_path,  map_location='cuda:0'))
-    model = model.to(device)
-
-    # make dataloader for test data
-    test_loader = make_actual_dataloader(test_data_path)
-    predictions = predict_test_data(model, test_loader)
-    path_list = os.listdir(test_data_path)
-    path_list.sort(key=lambda x:int(x.split('.')[0]))
-    dfDict = { 
-        'file': path_list,
-        'species': predictions
-    }
-    left_car = []
-    right_car = []
-    straight_car = []
-    for key,value in zip(path_list, predictions):
-        if value == 'left':
-            left_car.append(int(key.split('.')[0]))
-        elif value == 'right':
-            right_car.append(int(key.split('.')[0]))
-        elif value == 'straight':
-            straight_car.append(int(key.split('.')[0]))
-    
-    df = pd.DataFrame(dfDict)
-    csv_file_path = os.path.join(output_path, name+"_predictions.csv")
-    df.to_csv(csv_file_path, index=False)
-
-    print(f"Predictions saved to {csv_file_path}")
-    return [left_car,right_car,straight_car]
-
 
 def main():
     base_path = os.path.dirname(os.path.abspath(__file__))
