d7c9bb60d2d348fd50af1ff2fb86af162708eac0
weikikiii <132288830+weikikiii@users.noreply.github.com>
2024-12-22 10:24:42 +0800

    Update train.py

--- light_model_train/train.py
+++ light_model_train/train.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 
 
 model = YOLO('yolov8n.pt')
-model.train(data = '/ultralytics/cfg/datasets/car_light_dataset.yaml', epochs=20)
+model.train(data = '/ultralytics/cfg/datasets/car_light_dataset.yaml', epochs=20) //資料路徑, 訓練次數
